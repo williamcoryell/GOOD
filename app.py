a1b42a0ffbcac29854721b4dce919ad5ef924592
MyungLee11111 <myunglee049@gmail.com>
2025-01-23 09:52:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,66 @@
-from flask import Flask, render_template, request # type: ignore
+from flask import Flask, render_template, request  # type: ignore
+import os
 from GoogleNews import GoogleNews
 from newspaper import Article
 import pandas as pd
+from gnews import GNews
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import asyncio
+from twikit import Client
 
-def get_urls(topic):
-    googlenews=GoogleNews()
+USERNAME = 'mickey76148'
+PASSWORD = 'twitterScrapingTool'
+
+topicCHOSEN = ""
+URLS = []
+
+def get_urls_news(topic):
+    global quickDict
+    start_time = time.time()
+    googlenews = GoogleNews()
     googlenews.search(topic)
-    result=googlenews.result()
-    df=pd.DataFrame(result)
-    return [df["link"][i] for i in range(10)]
+    lstOfdf = []
+    for i in range(4):
+        result = googlenews.page_at(i)
+        lstOfdf.append(pd.DataFrame(result))
+    retLst = []
+    quickDict = {}
+    for df in lstOfdf:
+        for i in range(len(df["link"])):
+            url = df["link"][i][:df["link"][i].rfind("&ved=")] + "/"
+            if url is None or url in retLst:
+                continue
+            article = Article(url)
+            try:
+                article.download()
+                article.parse()
+                quickDict[url] = article.text
+                if url is not None:
+                    retLst.append(url)
+            except:
+                print(url)
+    print(time.time() - start_time)
+    return retLst
+
+async def get_urls_twitter(topic):
+    global quickDict
+    quickDict = {}
+    client = Client('en-US')
+    await client.login(
+        auth_info_1=USERNAME,
+        password=PASSWORD
+    )
+    retLst = []
+    tweets = await client.search_tweet(topic, 'Latest')
+    for tweet in tweets:
+        quickDict[tweet.user.name] = tweet.text
+        retLst.append(tweet.user.name)
+    return retLst
 
 def get_article_content(url):
-    article = Article(url)
-    return article.text
+    return quickDict[url]
 
 app = Flask(__name__)
 
@@ -22,15 +70,39 @@ def hello_world():
 
 @app.route('/', methods=['POST'])
 def text():
+    global topicCHOSEN
+    global URLS
     topic = request.form['topic']
-    urls = get_urls(topic)
-    return render_template("index.html", topic=topic, go=True, urls=urls)
+    source = request.form['source']
+    topicCHOSEN = topic
+
+    if source == "news":
+        urls = get_urls_news(topic)
+    elif source == "twitter":
+        urls = asyncio.run(get_urls_twitter(topic))
+    else:
+        urls = []
+
+    URLS = urls
+    return render_template("index.html", topic=topic, go=True, urls=urls, source=source)
 
 @app.route('/urlInfo', methods=['POST'])
 def topic():
     topic = request.form['topicChose']
     texter = get_article_content(topic)
-    return render_template("index.html", specificTopic=topic, topicText = texter)
-    
+    return render_template("index.html", specificTopic=topic, topicText=texter)
+
+@app.route('/graph', methods=['POST'])
+def graph():
+    global URLS   
+    print(URLS)
+    return render_template('graph.html', urls = URLS, specificTopic=topicCHOSEN)
+
+@app.route('/sent', methods=['POST'])
+def sentiment():
+    return render_template("sent.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
+    
+
